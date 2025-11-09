279f87a4ff6fdacc4dbf80a46858ad188cbf463e
Kien Mai <kientri098@gmail.com>
2025-11-09 22:58:29 +0700

    delete node modules, refactor db

--- file chứa query tạo DB/create table.sql
+++ file chứa query tạo DB/create table.sql
@@ -3,9 +3,11 @@ CREATE DATABASE test;
 -- Account table
 CREATE TABLE Account(
   id VARCHAR(36) PRIMARY KEY,
-  username VARCHAR(32) NOT NULL UNIQUE,
+  username VARCHAR(32) NOT NULL,
   email VARCHAR(32) NOT NULL UNIQUE,
-  password VARCHAR(64) NOT NULL,
+  googleId VARCHAR(255) NULL UNIQUE,
+  picture VARCHAR(500) NULL,
+  password VARCHAR(64) NULL,
   classroomLimit INT DEFAULT(1)
 );
 -- Friend table
@@ -63,29 +65,6 @@ CREATE TABLE Classroom(
   FOREIGN KEY (teacherId) REFERENCES Account(id),
   FOREIGN KEY (roadmapId) REFERENCES Roadmap(id)
 );
--- Quiz table
-CREATE TABLE Quiz(
-  id VARCHAR(36) PRIMARY KEY,
-  topicId VARCHAR(36),
-  classroomId VARCHAR(36),
---   FOREIGN KEY (topicId) REFERENCES Node(id),
-  FOREIGN KEY (classroomId) REFERENCES Classroom(id)
-);
--- Question table
-CREATE TABLE Question(
-  id VARCHAR(36) PRIMARY KEY,
-  quizId VARCHAR(36),
-  FOREIGN KEY (quizId) REFERENCES Quiz(id),
-  content VARCHAR(255) -- tăng lên
-);
--- answer table
-CREATE TABLE Answer(
-  id VARCHAR(36) PRIMARY KEY,
-  questionId VARCHAR(36),
-  FOREIGN KEY (questionId) REFERENCES Question(id),
-  answer VARCHAR(255), -- tăng lên
-  isCorrect TINYINT(1)
-);
 -- StudentClassroom table
 CREATE TABLE StudentClassroom(
   id VARCHAR(36) PRIMARY KEY,
@@ -93,17 +72,6 @@ CREATE TABLE StudentClassroom(
   studentId VARCHAR(36),
   FOREIGN KEY (classroomId) REFERENCES Classroom(id),
   FOREIGN KEY (studentId) REFERENCES Account(id)
-);
--- DoQuiz table
-CREATE TABLE DoQuiz(
-  id VARCHAR(36) PRIMARY KEY,
-  quizId VARCHAR(36),
-  accountId VARCHAR(36),
-  classroomId VARCHAR(36),
-  FOREIGN KEY (quizId) REFERENCES Quiz(id),
-  FOREIGN KEY (accountId) REFERENCES Account(id),
-  FOREIGN KEY (classroomId) REFERENCES Classroom(id),
-  score INT
 );
 -- MarkRoadmap table
 CREATE TABLE MarkRoadmap(
@@ -175,16 +143,38 @@ CREATE TABLE Notification (
   FOREIGN KEY (senderId) REFERENCES Account(id),
   FOREIGN KEY (receiverId) REFERENCES Account(id)
 );
+CREATE TABLE IF NOT EXISTS RefreshToken (
+    id VARCHAR(36) PRIMARY KEY,
+    accountId VARCHAR(36) NOT NULL,
+    token TEXT NOT NULL,
+    expiresAt DATETIME NOT NULL,
+    createdAt DATETIME DEFAULT CURRENT_TIMESTAMP,
+    isRevoked TINYINT(1) DEFAULT 0,
+    deviceInfo VARCHAR(255) NULL,
+    ipAddress VARCHAR(45) NULL,
+    
+    INDEX idx_account_id (accountId),
+    INDEX idx_expires_at (expiresAt),
+    INDEX idx_token (token(255))
+);
+-- Add foreign key constraint separately
+ALTER TABLE RefreshToken 
+ADD CONSTRAINT fk_refresh_token_account 
+FOREIGN KEY (accountId) REFERENCES Account(id) ON DELETE CASCADE;
 
+-- Add index for faster token lookup
+CREATE INDEX idx_account_token ON RefreshToken(accountId, isRevoked);
 -- ============================================ Mock Data =========================================
 -- Account
 INSERT INTO account (id, username, email, password, classroomLimit)
 VALUES
+('661b2db8-e1af-9824-f5c2-721965eaebb6', 'kitdev', 'kitdevhoang@gmail.com', '$2b$10$T9vDA8xKZ5Q9S5C3.1imVuKuL7buyHA1hY8/GYMrHX1V35cXN1ACW', 1),
 ('30254e37-7f55-11', 'hoang4', 'levanviethoang05@gmail.com', '$10$Yi5hDoADwkRY7KwGbu6CUeZ.3sIsJWdtW05Rqof98sSJlbAz9aaYC', 1),
 ('30254f66-7f55-11', 'hoang3', 'levanviethoang03@gmail.com', '$10$Yi5hDoADwkRY7KwGbu6CUeZ.3sIsJWdtW05Rqof98sSJlbAz9aaYC', 1),
 ('30254fca-7f55-11', 'hoang2', 'levanviethoang02@gmail.com', '$10$Yi5hDoADwkRY7KwGbu6CUeZ.3sIsJWdtW05Rqof98sSJlbAz9aaYC', 1),
 ('30255001-7f55-11', 'hoang1', 'levanviethoang01@gmail.com', '$10$Yi5hDoADwkRY7KwGbu6CUeZ.3sIsJWdtW05Rqof98sSJlbAz9aaYC', 1),
-('30255039-7f55-11', 'kien', 'kien@gmail.com', '$10$Yi5hDoADwkRY7KwGbu6CUeZ.3sIsJWdtW05Rqof98sSJlbAz9aaYC', 1);
+('30255039-7f55-11', 'kien', 'kien@gmail.com', '$10$Yi5hDoADwkRY7KwGbu6CUeZ.3sIsJWdtW05Rqof98sSJlbAz9aaYC', 1),
+('a1a24ce2-9f76-32d5-61ea-0389ba090175', 'Kiên Mai', 'imosciencemath@gmail.com', '$2b$10$k8DdCRsv/q8oOo8dRMjSHur0rK0VKtMKGQkqOTf8CIXd4tyBSCs9O', 1);
 -- Profile
 INSERT INTO profile (id, accountId, fullname, github, linkedin, avatar)
 VALUES
@@ -192,7 +182,9 @@ VALUES
 ('953e78ac-12a1-4f', '30254f66-7f55-11', 'Nguyễn Minh Hoàng', 'https://github.com/hoang03', 'https://linkedin.com/in/minh-hoang-nguyen', 'https://avatar.githubusercontent.com/hoang03'),
 ('c6cc161d-e1a2-48', '30254fca-7f55-11', 'Trần Đức Hoàng', 'https://github.com/hoang02', NULL, NULL),
 ('f339880e-97ce-44', '30255001-7f55-11', 'Phạm Văn Hoàng', NULL, 'https://linkedin.com/in/van-hoang-pham', NULL),
-('4cc488d6-0155-4f', '30255039-7f55-11', 'Mai Đức Kiên', 'https://github.com/kienduc', 'https://linkedin.com/in/duc-kien-mai', NULL);
+('4cc488d6-0155-4f', '30255039-7f55-11', 'Mai Đức Kiên', 'https://github.com/kienduc', 'https://linkedin.com/in/duc-kien-mai', NULL),
+('e9835529-f28b-569e-2a53-9bcf52d3f708', 'a1a24ce2-9f76-32d5-61ea-0389ba090175', 'Kiên Mai', NULL, NULL, 'https://deadline.com/wp-content/uploads/2024/09/Pokemon-Mini-Series-Aim-to-Be-a-Pokemon-Master-Ep-1-11.jpg?w=800'),
+('6fa334e9-2e68-2a1d-c565-cf18ede3e36e', '661b2db8-e1af-9824-f5c2-721965eaebb6', 'kitdev', NULL, NULL, NULL);
 -- Team
 INSERT INTO team (id, name)
 VALUES
@@ -218,4 +210,25 @@ VALUES
 ('9b59c7cc-028e-46', '30254fca-7f55-11', NULL, 'DevOps Path', 'DevOps practices and tools mastery',1,0,0),
 ('63945fb4-09a3-44', '30255001-7f55-11', NULL, 'Data Science', 'Data science and machine learning roadmap',1,0,0),
 ('7fa96882-fbdd-47', '30255001-7f55-11', NULL, 'Mobile Dev', 'Mobile app development guide',1,0,0),
-('d01c5a59-f3cf-44', '30255039-7f55-11', NULL, 'Web Design', 'UI/UX design principles and tools',1,0,0);
+('d01c5a59-f3cf-44', '30255039-7f55-11', NULL, 'Web Design', 'UI/UX design principles and tools',1,0,0),
+('77f71118-f6f5-7aaf-d932-5a56b8d247c0','a1a24ce2-9f76-32d5-61ea-0389ba090175',NULL,'1','1',1,0,0);
+-- Classroom
+INSERT INTO `classroom` VALUES ('ce0ae0c4-d9db-f729-2706-963fa0395113', 'a1a24ce2-9f76-32d5-61ea-0389ba090175', 'lh', 'lh','77f71118-f6f5-7aaf-d932-5a56b8d247c0');
+-- Post
+
+INSERT INTO `post` VALUES ('1', 'a1a24ce2-9f76-32d5-61ea-0389ba090175', 'ce0ae0c4-d9db-f729-2706-963fa0395113', '2025-09-09',  '📢📢📢 Điểm tổng kết môn học 📢📢📢\r\n👉 Các em xem điểm trong file đính kèm nhé, điểm cả 3 lớp thầy để chung 1 file.\r\n👉 Điểm cá nhân nằm trong sheet riêng của từng lớp\r\n👉 Điểm đồ án, gồm 2 cột GK CK nằm trong sheet `G-Project-Agg`, và có chuyển sang 2 cột GK CK ');
+INSERT INTO `post` VALUES ('2', 'a1a24ce2-9f76-32d5-61ea-0389ba090175', 'ce0ae0c4-d9db-f729-2706-963fa0395113', '2025-09-09',  'Từ thứ 7 tới giờ có rất nhiều bạn quan tâm việc khi nào nộp đồ án, khi còn học thầy đã thông báo là 1 tuần sau khi thi sẽ nộp đồ án mà các em cứ hỏi mãi.\r\n\r\n- Nhóm nào làm xong rồi thì cứ để đó, khi nào có link nộp thì nộp\r\n- Nhóm nào làm chưa xong thì lo');
+INSERT INTO `post` VALUES ('3c7013e3-ce4b-7761-114a-7c54d020ed2e', 'a1a24ce2-9f76-32d5-61ea-0389ba090175', 'ce0ae0c4-d9db-f729-2706-963fa0395113', '2025-09-26', '<p>ádasdsda</p>');
+INSERT INTO `post` VALUES ('d1a3460d-e7e2-c297-cf40-6995df01fc38', 'a1a24ce2-9f76-32d5-61ea-0389ba090175', 'ce0ae0c4-d9db-f729-2706-963fa0395113', '2025-09-26', '<p>ádasdasdasd</p>');
+-- Comment
+INSERT INTO `comment` VALUES ('1', 'a1a24ce2-9f76-32d5-61ea-0389ba090175', 'ce0ae0c4-d9db-f729-2706-963fa0395113', '1', '2025-09-09', 'Làm hết em');
+INSERT INTO `comment` VALUES ('2', 'a1a24ce2-9f76-32d5-61ea-0389ba090175', 'ce0ae0c4-d9db-f729-2706-963fa0395113', '2', '2025-09-09', 'Clip quá trình thì chứa đầy đủ quá trình làm bài là được');
+INSERT INTO `comment` VALUES ('3', 'a1a24ce2-9f76-32d5-61ea-0389ba090175', 'ce0ae0c4-d9db-f729-2706-963fa0395113', '1', '2025-09-09', 'Làm hết em oke thay');
+INSERT INTO `comment` VALUES ('4', 'a1a24ce2-9f76-32d5-61ea-0389ba090175', 'ce0ae0c4-d9db-f729-2706-963fa0395113', '1', '2025-09-08', 'kkkkk');
+INSERT INTO `comment` VALUES ('5', 'a1a24ce2-9f76-32d5-61ea-0389ba090175', 'ce0ae0c4-d9db-f729-2706-963fa0395113', '1', '2025-09-07', 'iiii');
+-- studentClassroom
+INSERT INTO `studentclassroom` VALUES ('09d65ee2-5313-82b1-7389-3b662f357b0d', 'ce0ae0c4-d9db-f729-2706-963fa0395113', '661b2db8-e1af-9824-f5c2-721965eaebb6');
+-- Notification
+INSERT INTO `notification` VALUES ('20a1f6cd-2e67-431a-e2a4-495bae748eda', '661b2db8-e1af-9824-f5c2-721965eaebb6', 'a1a24ce2-9f76-32d5-61ea-0389ba090175', '<p>ádasdasdasd</p>', 0, '2025-09-26T10:24',NULL);
+INSERT INTO `notification` VALUES ('881c99ed-6240-6eea-7950-43f09f095c9a', '661b2db8-e1af-9824-f5c2-721965eaebb6', 'a1a24ce2-9f76-32d5-61ea-0389ba090175', '<p>ádasdsda</p>', 0, '2025-09-26T09:54',NULL);
+INSERT INTO `notification` VALUES ('8ee0a30a-16b9-4eb3-eca6-d4074b78068b', '661b2db8-e1af-9824-f5c2-721965eaebb6', 'a1a24ce2-9f76-32d5-61ea-0389ba090175', '<p>ádasdasdasdasd</p>', 0, '2025-09-26T10:24',NULL);
